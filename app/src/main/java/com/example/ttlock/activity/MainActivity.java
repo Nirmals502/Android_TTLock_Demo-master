08d2ff2731d600fd63e92278bc571c64cedf80fb
Nirmal <nirmals502@gmail.com>
2018-08-10 16:20:58 +0530

    update

--- app/src/main/java/com/example/ttlock/activity/MainActivity.java
+++ app/src/main/java/com/example/ttlock/activity/MainActivity.java
@@ -105,6 +105,7 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
     private void initUI() {
         setSupportActionBar(toolbar);
 
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
